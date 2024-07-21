4b5c8bcdabd8dd9bc69b0e10b2e9ca1cdabdf659
Anubhav Narayan Singh <122772894+Anubhav-GEU@users.noreply.github.com>
2024-07-21 20:07:39 +0530

    Create partitionList.cpp

---
+++ Linked List/partitionList.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        if (head == NULL || head->next == NULL) return head;
+        ListNode* ss=NULL,*se=NULL, *gs=NULL, *ge=NULL;
+        ListNode* temp = head;
+        while (temp) {
+            if (temp->val < x) {
+                if (ss == NULL) ss = se = temp;
+                else {
+                    se->next = temp;
+                    se = se->next;
+                }
+            }
+            else {
+                if (gs == NULL) gs = ge = temp;
+                else {
+                    ge->next = temp;
+                    ge = ge->next;
+                }
+            }
+            temp = temp->next;
+        }
+        if (gs != NULL) ge->next = NULL;
+        if (ss == NULL) return gs;
+        se->next = gs;
+        return ss;
+    }
+};
